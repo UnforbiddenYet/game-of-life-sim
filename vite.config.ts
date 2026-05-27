4e127a949a18db88b5ba3d2f67375b461a693279
Dmytro <polder-grapnel0e@icloud.com>
2026-05-27 15:48:29 +0200

    chore: update vite.config.ts for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/game-of-life-sim/",
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
   },
-})
+});
